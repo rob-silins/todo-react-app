3d80379afae0a57690d1673427d12db2caa875ab
Roberts Silins <robiss46@gmail.com>
2020-10-11 12:55:03 +0300

    Todo react app

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import './App.css';
+import Header from './Components/Layout/Header';
+import Todos from './Components/Todos';
+import AddTodo from './Components/Layout/AddTodo';
+import About from './Components/Pages/About';
+import axios from 'axios';
+
+class App extends Component 
+{
+    state = {
+    todos: []
+  }
+
+  componentDidMount (){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => this.setState({ todos: res.data }))
+  }
+
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })
+  });
+  }
+
+  delTodo = (id) => {
+    axios.delete(`
+    https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then(res =>this.setState({todos:
+      [...this.state.todos.filter
+      (todo => todo.id !== id )]}));
+  }
+
+  AddTodo = (title) => {
+    if(title !=="")
+    axios.post('https://jsonplaceholder.typicode.com/todos',
+    {title,
+    completed: false
+  })
+  .then(res => this.setState({ 
+    todos: [...this.state.todos, res.data]}) );
+  
+  }
+
+  render()
+  {
+    return (
+    <Router>
+      <div className="App">
+        <div className="container">
+          <Header />
+          <Route exact path="/" render={()=>(
+            <React.Fragment>
+            <AddTodo AddTodo = {this.AddTodo}/>
+            <Todos todos={this.state.todos} 
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}/>
+            </React.Fragment>
+          )} />
+          <Route path="/about" component={About} />
+        </div>
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Pages/About.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <h1 style = {aboutStyle}>About this page</h1>
+            <div style = {aboutTextStyle}>
+            <p>This is just something I tried to make while watching a YouTube tutorial.</p>
+            <p>The gibberish tasks that are pre-loaded are imported from 
+                <a href='https://jsonplaceholder.typicode.com' target='blank' >
+                <i> Json Placeholder.</i>
+                </a>
+            </p>
+            <p>You can add new tasks and by checking the box next to it, it will cross off the task.</p>
+            <p>If you don't need the task anymore, you can simply use the 
+                <button 
+                className='aboutBtnStyle'>REMOVE
+                </button>  
+                button to get rid of it completely.
+            </p>
+            <p>You don't need to worry about adding any blank tasks.</p>
+            </div>
+        </React.Fragment>
+    )
+}
+
+const aboutStyle ={
+    textAlign: 'center',
+    padding: '20px'
+}
+
+const aboutTextStyle ={
+    textAlign: 'center',
+    padding: '20px',
+    fontSize: '18px',
+    lineHeight:'2'
+}
+
+export default About;
